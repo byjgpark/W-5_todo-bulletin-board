e14a462ba676ad28f569b40339e03d85713753fd
Jungi Park <byjungy93@gmail.com>
2022-08-06 13:52:38 +0900

    add comment_0.1V

---
+++ src/component/detail/Detail.jsx
@@ -0,0 +1,12 @@
+import Comment from "../comment/Comment"
+
+function Detail() {
+  return (
+  <div className="App">
+    <h1>hell12</h1>
+    <Comment></Comment>
+  </div>
+  )
+}
+
+export default Detail;
