8184c66352227907d9c46725e737833be47f380c
jaseongKim <jaseong4281@gmail.com>
2022-08-06 14:23:22 +0900

    220806 1차

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from "./logo.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Detail from "./component/detail/Detail";
+import TodoList from "./component/todoList/TodoList";
 
 function App() {
-  return <div className="App">
-    <h1>hello</h1>
-  </div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<TodoList />} />
+          <Route path="/detail/:id" element={<Detail />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
