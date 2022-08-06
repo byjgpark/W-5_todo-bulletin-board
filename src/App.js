fa11b7bd9ff067bc3fcbe6222af45814c7284333
Rita Soojin Woo <linawoo@naver.com>
2022-08-06 13:55:23 +0900

    button_router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import logo from "./logo.svg";
+import React from "react";
+import Router from "./shared/Router";
+// import { useState } from "react";
 import "./App.css";
+import Form from "./component/form/Form";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
+
   return (
-    <div className="App">
-      <h1>hello soojin</h1>
+    <div>
+      <div>
+        <h1>B반 대나무숲</h1>
+      </div>
+      <div>
+        <h1>무엇을 할까요?</h1>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            navigate("/form");
+          }}
+        >
+          게시글 등록하기
+        </button>
+        <button>게시글 목록</button>
+      </div>
+      {/* <Router />; */}
     </div>
   );
 }
-
+<Router />;
 export default App;
